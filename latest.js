62588f903c8093b619f4aa3df077b184ee1a37f5
Yeonwoon JUNG <flow3r@gmail.com>
2016-02-29 02:30:57 +0100

    minor

--- latest.js
+++ latest.js
@@ -3,7 +3,7 @@ var remote_filename = 'namuwiki-160202.sql';
 var local = document.querySelector('#latest');
 var local_filename = local.getAttribute('data-filename');
 if (local_filename == remote_filename) {
-  local.innerHTML = '✅ 최신버전입니다.';
+  local.innerHTML = '✅ 최신버전입니다';
 } else {
   var s='⚠️ 새 버전 내려받기: <code>' + remote_filename + '</code>';
   local.innerHTML = '<a href="modum://exec/download" class="exec">'+s+'</a>';
