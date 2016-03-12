363f9e512f469456fb7f89eabb810ca307f44dee
Yeonwoon JUNG <flow3r@gmail.com>
2016-03-13 00:32:21 +0100

    minor fix for v0.2.1

--- latest.js
+++ latest.js
@@ -4,10 +4,13 @@ if ('setLatest' in window) {
 }
 
 var local = document.querySelector('#latest');
-var local_filename = local.getAttribute('data-filename');
-if (local_filename == remote_filename) {
-  local.innerHTML = '✅ 최신버전입니다';
-} else {
-  var s='⚠️ 새 버전 내려받기: <code>' + remote_filename + '</code>';
-  local.innerHTML = '<a href="modum://exec/download" class="exec">'+s+'</a>';
+if (local) {
+  var local_filename = local.getAttribute('data-filename');
+  if (local_filename == remote_filename) {
+    local.innerHTML = '✅ 최신버전입니다';
+  } else {
+    var s='⚠️ 새 버전 내려받기: <code>' + remote_filename + '</code>';
+    local.innerHTML = '<a href="modum://exec/download" class="exec">'+s+'</a>';
+  }
 }
+
