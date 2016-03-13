0b8a14d38e7aea9cdc2870fe0e33b3a6eae0cc0e
Yeonwoon JUNG <flow3r@gmail.com>
2016-03-13 18:54:30 +0100

    rolling out database version change; v1 -> v2

---
+++ v2/latest.js
@@ -0,0 +1,16 @@
+var remote_filename = 'namuwiki-160307.sql';
+if ('setLatest' in window) {
+  setLatest(remote_filename);
+}
+
+var local = document.querySelector('#latest');
+if (local) {
+  var local_filename = local.getAttribute('data-filename');
+  if (local_filename == remote_filename) {
+    local.innerHTML = '✅ 최신버전입니다';
+  } else {
+    var s='⚠️ 새 버전 내려받기: <code>' + remote_filename + '</code>';
+    local.innerHTML = '<a href="modum://exec/download" class="exec">'+s+'</a>';
+  }
+}
+
